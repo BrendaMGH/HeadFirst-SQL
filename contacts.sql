794e0297283a83feb6e4c96873a2c753fcdade1a
Brenda <bmgonzalezh@uqvirtual.edu.co>
2020-10-20 17:00:01 -0500

    Chapter 8 - Joins

--- contacts.sql
+++ contacts.sql
@@ -1,4 +1,4 @@
---USE SM_04_BRENDA_GH
+USE SM_04_BRENDA_GH
 
 /*CREATE TABLE my_contacts
 (
@@ -71,20 +71,86 @@ DROP COLUMN location;*/
 ('Murphy','Donald','padraic@tikibeanlounge.com','M','1967-01-23','Computer Programmer','committed relationsh','RPG, anime','friends','NY','New York City'),
 ('Spatner','John','jpoet@objectville.net','M','1963-04-18','Salesman','married','poetry, screenwriting','nothing','NY','Woodstock');*/
 
-SELECT * FROM my_contacts;
+/*ALTER TABLE  my_contacts
+ADD interest3 VARCHAR(50);*/
+/*UPDATE my_contacts
+SET interest1 = SUBSTRING(interests, 1, (SELECT CHARINDEX(',',interests)-1)) FROM my_contacts;*/
+/*UPDATE my_contacts 
+SET interests = REPLACE(interests, SUBSTRING(interests, 1, (SELECT CHARINDEX(',',interests))),'') FROM my_contacts;*/
+/*ALTER TABLE my_contacts
+DROP COLUMN interests;*/
 
-      --------------------------------------------------------- interest ---------------------------------------------------------
+/*ALTER TABLE  my_contacts
+ADD seeking1 VARCHAR(50);*/
+/*UPDATE my_contacts
+SET seeking1 = SUBSTRING(seeking, 1, (SELECT CHARINDEX(',',seeking)-1)) FROM my_contacts;*/
+/*UPDATE my_contacts 
+SET seeking = REPLACE(seeking, SUBSTRING(seeking, 1, (SELECT CHARINDEX(',',seeking))),'') FROM my_contacts;*/
+
+      --------------------------------------------------------- New tables ---------------------------------------------------------
 
 /*CREATE TABLE interest(
 	int_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	interest VARCHAR(50) NOT NULL,
-	contact_id INT NOT NULL,
-	CONSTRAINT my_contacts_contact_id_fk
-	FOREIGN KEY (contact_id)
-	REFERENCES my_contacts (contact_id)
+	interest VARCHAR(50) NOT NULL
+);
+CREATE TABLE seeking(
+	seeking_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	seeking VARCHAR(50) NOT NULL
+);
+CREATE TABLE profession(
+	prof_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	profession VARCHAR(50) NOT NULL
+);
+CREATE TABLE status(
+	status_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	status VARCHAR(50) NOT NULL
 );*/
 
-SELECT * FROM interest;
+/*INSERT INTO interest (interest)
+	SELECT interest3 FROM my_contacts
+	WHERE interest3 IS NOT NULL
+	GROUP BY interest3;*/
+/*INSERT INTO profession (profession)
+	SELECT profession FROM my_contacts
+	GROUP BY profession
+	ORDER BY profession;*/
+/*INSERT INTO status (status)
+	SELECT status FROM my_contacts
+	GROUP BY status
+	ORDER BY status;*/
+/*INSERT INTO seeking (seeking)
+	SELECT seeking1 FROM my_contacts
+	WHERE seeking1 IS NOT NULL
+	GROUP BY seeking1
+	ORDER BY seeking1;*/
 
+/*ALTER TABLE my_contacts
+DROP COLUMN profession, status, seeking, seeking1, interest1, interest2, interest3;*/
 
+/*ALTER TABLE my_contacts
+ADD prof_id INT FOREIGN KEY REFERENCES profession (prof_id);
+ALTER TABLE my_contacts
+ADD status_id INT FOREIGN KEY REFERENCES status (status_id);
+UPDATE my_contacts
+SET prof_id = 6, status_id = 3 
+WHERE contact_id = 11;*/
 
+/*CREATE TABLE contact_interest(
+	contact_id INT NOT NULL FOREIGN KEY REFERENCES my_contacts (contact_id),
+	int_id  INT NOT NULL FOREIGN KEY REFERENCES interest (int_id)
+);
+CREATE TABLE contact_seeking(
+	contact_id INT NOT NULL FOREIGN KEY REFERENCES my_contacts (contact_id),
+	seeking_id  INT NOT NULL FOREIGN KEY REFERENCES seeking (seeking_id)
+);*/
+
+/*INSERT INTO contact_interest VALUES (1,5),(1,14),(2,9),(3,10),(4,8),(4,4),(4,18),(5,4),
+	(5,13),(6,7),(7,2),(7,16),(8,3),(9,1),(9,19),(10,7),(10,11),(11,6),(11,15);*
+INSERT INTO contact_seeking VALUES (1,11),(1,2),(2,10),(2,3),(3,7),(4,6),(5,4),(6,5),
+	(7,2),(8,1),(9,4),(10,2),(11,5)*/
+
+/*SELECT mc.contact_id, mc.first_name, mc.last_name, p.profession 
+FROM my_contacts AS mc
+INNER JOIN 
+profession AS p
+ON mc.prof_id = p.prof_id;*/ 
