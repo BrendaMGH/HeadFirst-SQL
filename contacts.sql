b6886f84bcc4749f45679f5aff87d7dedb7e1c55
Brenda <bmgonzalezh@uqvirtual.edu.co>
2020-10-21 09:32:38 -0500

    Chapter 9 - Subqueries

--- contacts.sql
+++ contacts.sql
@@ -154,3 +154,9 @@ FROM my_contacts AS mc
 INNER JOIN 
 profession AS p
 ON mc.prof_id = p.prof_id;*/ 
+
+SELECT mc.first_name firstname, mc.phone phone, s.status status
+FROM my_contacts mc INNER JOIN status s
+ON mc.status_id = s.status_id
+WHERE s.status = (SELECT TOP 1 s.status COUNT(s.status) AS mostOften FROM s 
+					GROUP BY s.status ORDER BY mostOften DESC);
